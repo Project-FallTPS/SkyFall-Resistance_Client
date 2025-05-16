c1d39565a4c40d55138a49589b43db030d3da075
Sumin Cha <109404269+goliot@users.noreply.github.com>
2025-05-16 12:43:29 +0900

    250516 / 풀매니저 수정

--- Assets/00. Core/Object Pool/EnemyPool/EnemyPoolManager.cs
+++ Assets/00. Core/Object Pool/EnemyPool/EnemyPoolManager.cs
@@ -3,16 +3,4 @@ using UnityEngine;
 
 public class EnemyPoolManager : BasePoolManager<EEnemyType, EnemyPoolInfo>
 {
-    private static EnemyPoolManager _instance;
-    public static EnemyPoolManager Instance
-    {
-        get
-        {
-            if(_instance == null)
-            {
-                _instance = FindAnyObjectByType<EnemyPoolManager>();
-            }
-            return _instance;
-        }
-    }
 }
