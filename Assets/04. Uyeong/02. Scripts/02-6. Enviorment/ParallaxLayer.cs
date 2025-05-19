79a0dcc4005cf9567202ad95d3abc34d8ac01474
wooing26 <104512612+wooing26@users.noreply.github.com>
2025-05-19 18:22:57 +0900

    Feat | Layer 이동 루프 구현
    레이어별 루프 여부 설정 및 오프셋을 이용한 이동 루프 가능 구현

--- Assets/04. Uyeong/02. Scripts/02-6. Enviorment/ParallaxLayer.cs
+++ Assets/04. Uyeong/02. Scripts/02-6. Enviorment/ParallaxLayer.cs
@@ -1,21 +1,33 @@
 using UnityEngine;
+using static ParallaxManager;
 
 public class ParallaxLayer : MonoBehaviour
 {
+    private Vector3 _startPosition;
     private float _moveSpeed = 5f;
+    private float _playerAreaRadius;
+    private float _loopOffset;
+    private bool _isLooping = true;
 
     private void Start()
     {
-        transform.localPosition = Vector3.zero;
+        _startPosition = transform.position;
     }
 
     private void Update()
     {
         transform.position += Vector3.up * _moveSpeed * Time.deltaTime;
+        if (_isLooping && transform.position.y >= _playerAreaRadius + _loopOffset)
+        {
+            transform.localPosition = _startPosition;
+        }
     }
 
-    public void SetMoveSpeed(float moveSpeed)
+    public void Initialize(ParallaxLayerSet parallaxLayerSet, float playerAreaRadius)
     {
-        _moveSpeed = moveSpeed;
+        _moveSpeed = parallaxLayerSet.MoveSpeed;
+        _playerAreaRadius = playerAreaRadius;
+        _loopOffset = parallaxLayerSet.LoopOffset;
+        _isLooping = parallaxLayerSet.IsLooping;
     }
 }
