3e09611b2818a86ddad5a48491fdc640875927b0
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2025-05-19 14:48:31 +0900

    Feat | VFX 풀링 구현

---
+++ Assets/03. Mingyu/02. Scripts/02-7. Environment/VFX/VFX.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using UnityEditor.EditorTools;
+using UnityEngine;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class VFX : MonoBehaviour
+{
+    private ParticleSystem[] _particleSystems;
+
+    private void Awake()
+    {
+        _particleSystems = GetComponentsInChildren<ParticleSystem>(true); // 자식 포함 전체 가져오기
+    }
+    public void OnGetFromPool(Vector3 position)
+    {
+        transform.position = position;
+        transform.rotation = Quaternion.identity;
+        StartCoroutine(VFXCoroutine());
+    }
+
+    public void OnGetFromPool(Vector3 position, Quaternion quaternion)
+    {
+        transform.position = position;
+        transform.rotation = quaternion;
+        StartCoroutine(VFXCoroutine());
+    }
+
+    private IEnumerator VFXCoroutine()
+    {
+        foreach (ParticleSystem ps in _particleSystems)
+        {
+            ps.Play();
+        }
+
+        // 모든 파티클이 다 죽을 때까지 기다림
+        bool isAlive;
+        do
+        {
+            isAlive = false;
+            foreach (ParticleSystem ps in _particleSystems)
+            {
+                if (ps.IsAlive(true)) // 자식 포함 체크
+                {
+                    isAlive = true;
+                    break;
+                }
+            }
+            yield return null;
+        } while (isAlive);
+
+    }
+}
