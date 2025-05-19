cbaa7e3ce37527c2d6e3c7f64a751836e5075d2e
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2025-05-19 16:44:46 +0900

    Feat | 적의 플레이어 추적 전략(일반 직선 궤적, 베지어 곡선 궤적) 구현

--- Assets/03. Mingyu/02. Scripts/02-3. Enemy/State/EnemyTraceState.cs
+++ Assets/03. Mingyu/02. Scripts/02-3. Enemy/State/EnemyTraceState.cs
@@ -1,13 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyTraceState : IEnemyState
 {
     private EnemyController _enemyController;
-    public EnemyTraceState(EnemyController enemyController)
+    private EnemyData _enemyData;
+    private ITraceStrategy _traceStrategy;
+    public EnemyTraceState(EnemyController enemyController, ITraceStrategy traceStrategy)
     {
         _enemyController = enemyController;
+        _enemyData = enemyController.EnemyData;
+        _traceStrategy = traceStrategy;
     }
     public void Enter()
     {
@@ -15,6 +17,12 @@ public class EnemyTraceState : IEnemyState
 
     public void Update()
     {
+        if (_enemyData.AttackableRange <=
+            Vector3.Distance(_enemyController.transform.position, _enemyController.Player.transform.position))
+        {
+            _enemyController.EnemyStateContext.ChangeState(_enemyController.EnemyStateDict[EEnemyState.Attack]);
+        }
+        _traceStrategy.Trace(_enemyController);
     }
 
     public void Exit()
--- Assets/03. Mingyu/02. Scripts/02-3. Enemy/Strategy/Trace/TraceNormal.cs
+++ Assets/03. Mingyu/02. Scripts/02-3. Enemy/Strategy/Trace/TraceNormal.cs
@@ -2,7 +2,18 @@ using UnityEngine;
 
 public class TraceNormal : ITraceStrategy
 {
-    public void Trace()
+    public void Trace(EnemyController self)
     {
+        Transform enemyTransform = self.transform;
+        Transform playerTransform = self.Player.transform;
+
+        Vector3 direction = (playerTransform.position - enemyTransform.position).normalized;
+
+        if (direction != Vector3.zero)
+        {
+            enemyTransform.rotation = Quaternion.LookRotation(direction);
+        }
+
+        enemyTransform.position += direction * self.EnemyData.MovementSpeed * Time.deltaTime;
     }
 }
