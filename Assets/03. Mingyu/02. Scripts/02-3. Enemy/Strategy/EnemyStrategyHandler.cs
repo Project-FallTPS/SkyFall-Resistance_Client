eae74232ba23f25fb5470a6e6bed615ea73cd8a5
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2025-05-19 12:31:39 +0900

    Feat | EnemyStrategyHandler Trace 전략 메서드 구현

--- Assets/03. Mingyu/02. Scripts/02-3. Enemy/Strategy/EnemyStrategyHandler.cs
+++ Assets/03. Mingyu/02. Scripts/02-3. Enemy/Strategy/EnemyStrategyHandler.cs
@@ -15,9 +15,26 @@ public class EnemyStrategyHandler : MonoBehaviour
     {
         _enemyTraceStrategySet.Add(new TraceNormal());
         _enemyTraceStrategySet.Add(new TraceBazier());
-
-
         _enemyAttackStrategyDict.Add(EEnemyType.Shooting, new AttackShooting());
         _enemyAttackStrategyDict.Add(EEnemyType.Bombing, new AttackBombing());
+    }
+
+    public ITraceStrategy PickTraceStrategy()
+    {
+        int rand = Random.Range(0, 100); // 0부터 99까지
+        foreach (var strategy in _enemyTraceStrategySet)
+        {
+            if (rand < 80 && strategy is TraceNormal)
+            {
+                return strategy;
+
+            }
+            else if (rand >= 80 && strategy is TraceBazier)
+            {
+                return strategy;
+
+            }
+        }
+        return null;
     }
 }
