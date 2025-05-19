5043e688824bad1ec2751aa1d63eb292bda5f913
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2025-05-19 16:02:33 +0900

    Feat | 슈팅 타입 적의 공격(총알 발사) 전략 구현

--- Assets/03. Mingyu/02. Scripts/02-3. Enemy/Strategy/Attack/Shooting/AttackShooting.cs
+++ Assets/03. Mingyu/02. Scripts/02-3. Enemy/Strategy/Attack/Shooting/AttackShooting.cs
@@ -4,6 +4,7 @@ public class AttackShooting : IAttackStrategy
 {
     public void Attack(Vector3 position, EnemyController self)
     {
-        DamageablePoolManager.Instance.GetObject(EDamageableType.EnemyBullet, position);
+        GameObject bullet = DamageablePoolManager.Instance.GetObject(EDamageableType.EnemyBullet, position);
+        bullet.GetComponent<Bullet>().Damage = self.EnemyData.AttackDamage;
     }
 }
--- Assets/03. Mingyu/02. Scripts/02-3. Enemy/Strategy/Attack/Shooting/Bullet.cs
+++ Assets/03. Mingyu/02. Scripts/02-3. Enemy/Strategy/Attack/Shooting/Bullet.cs
@@ -1,16 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
     [SerializeField]
     private float _speed = 10f;
+    [SerializeField]
+    private float _lifeTime = 5f;
+
+    private float _damage;
+    public float Damage { get => _damage; set => _damage = value; }
+    private void OnEnable()
+    {
+        StartCoroutine(LifeCycle());
+    }
 
     private void Update()
     {
         Move();
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(nameof(ETags.Player)))
+        {
+            IDamageable damageable = other.GetComponent<IDamageable>();
+            if (damageable != null)
+            {
+                damageable.TakeDamage(_damage);
+            }
+            DamageablePoolManager.Instance.ReturnObject(gameObject, EDamageableType.EnemyBullet);
+        }
+    }
+
+    private void OnDisable()
+    {
+        StopAllCoroutines();
+    }
+
     private void Move()
     {
         transform.Translate(Vector3.forward * _speed * Time.deltaTime);
+    }
+
+    private IEnumerator LifeCycle()
+    {
+        yield return new WaitForSeconds(_lifeTime);
+        DamageablePoolManager.Instance.ReturnObject(gameObject, EDamageableType.EnemyBullet);
     }
 }
