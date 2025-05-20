7d6f1f31e2bd100207cb7b49c95af701a6c45ca4
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2025-05-20 10:19:45 +0900

    Feat | EnemyDataSO 설계 및 EnemyData Monobehaviour 비상속으로 변경

--- Assets/03. Mingyu/02. Scripts/02-3. Enemy/State/EnemyController.cs
+++ Assets/03. Mingyu/02. Scripts/02-3. Enemy/State/EnemyController.cs
@@ -17,19 +17,29 @@ public class EnemyController : MonoBehaviour
     private CapsuleCollider _enemyCollider;
     public CapsuleCollider EnemyCollider => _enemyCollider;
 
+    [Header("Datas")]
+    [SerializeField]
+    private EEnemyType _enemyType;
+    [SerializeField]
+    private EnemyDataSO _enemyDataSO;
+
     private EnemyData _enemyData;
     public EnemyData EnemyData { get => _enemyData; set => _enemyData = value; }
 
-    [Header("References")]
+    [Header("External References")]
     private GameObject _player;
     public GameObject Player => _player;
+
 
     private void Awake()
     {
         _enemyStateContext = new EnemyStateContext(this);
         _enemyStateDict = new Dictionary<EEnemyState, IEnemyState>();
+
         _enemyCollider = GetComponent<CapsuleCollider>();
-        _enemyData = GetComponent<EnemyData>();
+
+        _enemyData = _enemyDataSO.GetEnemyData(_enemyType);
+
         _player = GameObject.FindGameObjectWithTag(nameof(ETags.Player));
     }
 
@@ -46,7 +56,6 @@ public class EnemyController : MonoBehaviour
         _enemyStateDict.Add(EEnemyState.Attack, new EnemyAttackState(this, EnemyStrategyHandler.Instance.EnemyAttackStrategyDict[_enemyData.EnemyType]));
         _enemyStateDict.Add(EEnemyState.Damaged, new EnemyDamagedState(this));
         _enemyStateDict.Add(EEnemyState.Die, new EnemyDieState(this));
-
         _enemyStateContext.ChangeState(_enemyStateDict[EEnemyState.Trace]);
     }
 
