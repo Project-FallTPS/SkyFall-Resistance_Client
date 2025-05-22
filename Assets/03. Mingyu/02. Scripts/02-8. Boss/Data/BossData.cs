87655424f1116186fa649a3c09a40d8510641fcb
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2025-05-22 10:32:44 +0900

    Feat | 보스 데이터 설계

---
+++ Assets/03. Mingyu/02. Scripts/02-8. Boss/Data/BossData.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+[System.Serializable]
+public class BossData
+{
+    private EBossType _bossType;
+    public EBossType BossType { get => _bossType; set => _bossType = value; }
+
+    [Header("Basic")] 
+    public float MaxHealth;
+
+    [SerializeField] 
+    private float _currentHealth;
+    public float CurrentHealth
+    {
+        get => _currentHealth;
+        set => _currentHealth = Mathf.Clamp(value, 0f, MaxHealth);
+    }
+
+    public float MoveSpeed;
+
+
+    [Header("Attack")]
+    public float AttackDamage;
+    public float AttackCooltime;
+}
