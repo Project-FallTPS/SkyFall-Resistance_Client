c6216e8fd8f1e3f507ef639789904ef015fbbc3d
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2025-05-27 11:02:38 +0900

    Style | 미사용 네임스페이스 참조 제거

--- Assets/03. Mingyu/02. Scripts/02-8. Boss/Behavior Tree/BossRushAction.cs
+++ Assets/03. Mingyu/02. Scripts/02-8. Boss/Behavior Tree/BossRushAction.cs
@@ -4,7 +4,6 @@ using Unity.Behavior;
 using UnityEngine;
 using Action = Unity.Behavior.Action;
 using Unity.Properties;
-using Unity.VisualScripting;
 
 [Serializable, GeneratePropertyBag]
 [NodeDescription(name: "BossRush", story: "돌진 공격(페이즈2)", category: "Action", id: "e22fa8e1d643ec7c27a970890fa80d0b")]
