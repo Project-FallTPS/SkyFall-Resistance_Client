6e7d0e093132b09937fe93f34076d3e656e712cb
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2025-05-29 17:30:39 +0900

    Refactor | 쉴드효과 Trail이 효과받는 적을 끝까지 추격하도록 변경, 해당 로직 별도 코드 분리

--- Assets/03. Mingyu/02. Scripts/02-3. Enemy/Strategy/Attack/Supporting/AttackSupporting.cs
+++ Assets/03. Mingyu/02. Scripts/02-3. Enemy/Strategy/Attack/Supporting/AttackSupporting.cs
@@ -28,46 +28,23 @@ public class AttackSupporting : IAttackStrategy, ITransitionStrategy
         foreach (Collider hitCollider in hitColiiders)
         {
             if (hitCollider.CompareTag(nameof(ETags.Enemy)) &&
-                hitCollider.TryGetComponent<EnemyController>(out EnemyController enemyController) &&
-                !enemyController.EnemyData.IsShieldActive)
+                hitCollider.TryGetComponent<EnemyController>(out EnemyController enemyController))
             {
                 enemyController.ActivateShield();
-                ShieldTrailEffect(self, enemyController);
+                MakeShieldTrail(self, enemyController);
             }
         }
     }
 
-    private void ShieldTrailEffect(EnemyController from, EnemyController to)
+    private void MakeShieldTrail(EnemyController from, EnemyController to)
     {
         Vector3 start = from.transform.position;
-        Vector3 end = to.transform.position;
 
-        TrailRenderer trail =
+        TrailMovement trailMovement =
             VFXPoolManager.Instance
                 .GetObject(EVFXType.EnemySupportTypeShieldTrail, start, quaternion.identity)
-                .GetComponent<TrailRenderer>();
-
-        float distance = Vector3.Distance(start, end);
-        float duration = distance / 5f;
-
-        from.StartCoroutineInEnemyState(MoveShieldTrail(trail, start, end, duration));
+                .GetComponent<TrailMovement>();
+        trailMovement.SetStartPositionAndTarget(from.transform.position, to.transform);
     }
-
-    private IEnumerator MoveShieldTrail(TrailRenderer trail, Vector3 start, Vector3 end, float duration)
-    {
-        if (ReferenceEquals(trail, null))
-        {
-            yield break;
-        }
-        
-        float time = 0f;
-        while (time < duration)
-        {
-            trail.transform.position = Vector3.Lerp(start, end, time / duration);
-            time += Time.deltaTime;
-            yield return null;
-        }
-        yield return new WaitForSeconds(0.3f);
-        VFXPoolManager.Instance.ReturnObject(trail.gameObject, EVFXType.EnemySupportTypeShieldTrail);
-    }
+    
 }
