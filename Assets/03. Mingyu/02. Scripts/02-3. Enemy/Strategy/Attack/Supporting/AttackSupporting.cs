7aa790ffc34a6beb9e57864fc0eb8cb45c9c14a7
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2025-05-29 14:37:23 +0900

    Feat | 지원 타입 공격(지원)전략 구현

--- Assets/03. Mingyu/02. Scripts/02-3. Enemy/Strategy/Attack/Supporting/AttackSupporting.cs
+++ Assets/03. Mingyu/02. Scripts/02-3. Enemy/Strategy/Attack/Supporting/AttackSupporting.cs
@@ -1,8 +1,36 @@
 using UnityEngine;
 
-public class AttackSupporting : IAttackStrategy
+public class AttackSupporting : IAttackStrategy, ITransitionStrategy
 {
     public void Attack(EnemyController self)
     {
+        ApplyShield(self);
+    }
+
+    public bool CanChangeToAttackState(EnemyController self)
+    {
+        return self.EnemyData.NextAttackableTime <= Time.time;
+    }
+
+    public bool CanChangeToTraceState(EnemyController self)
+    {
+        return true;
+    }
+    
+    private void ApplyShield(EnemyController self)
+    {
+        Collider[] hitColiiders = 
+            Physics.OverlapSphere
+                (self.transform.position, self.EnemyData.ShieldBuffRadius);
+        
+        foreach (Collider hitCollider in hitColiiders)
+        {
+            if (hitCollider.CompareTag(nameof(ETags.Enemy)) &&
+                hitCollider.TryGetComponent<EnemyController>(out EnemyController enemyController) &&
+                !enemyController.EnemyData.IsShieldActive)
+            {
+                enemyController.ActivateShield();
+            }
+        }
     }
 }
