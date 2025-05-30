14cd4ffc38490b31add5941eeb51276969932539
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2025-05-30 14:18:29 +0900

    Feat | 컷씬에서의 비동기씬 로드 및 컷씬 종료후 씬 전환 구현

--- Assets/03. Mingyu/02. Scripts/02-1. Core/SceneTransitionManager.cs
+++ Assets/03. Mingyu/02. Scripts/02-1. Core/SceneTransitionManager.cs
@@ -21,7 +21,8 @@ public class SceneTransitionManager : Singleton<SceneTransitionManager>
         SceneManager.LoadScene(nameof(sceneName));
     }
 
-    public IEnumerator LoadSceneAsync(int sceneIndex, Action onLoading = null, Action onComplete = null)
+    public IEnumerator LoadSceneAsync(int sceneIndex, 
+        Func<bool> canActivateScene = null, Action onLoading = null, Action onComplete = null)
     {
         AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(sceneIndex);
         if (asyncOperation != null)
@@ -31,49 +32,70 @@ public class SceneTransitionManager : Singleton<SceneTransitionManager>
             while (!asyncOperation.isDone)
             {
                 onLoading?.Invoke();
+
                 if (0.9f <= asyncOperation.progress)
                 {
                     onComplete?.Invoke();
-                    asyncOperation.allowSceneActivation = true;
-                }
 
+                    // 외부 조건이 true가 되어야 씬 전환 진행
+                    if (canActivateScene != null && canActivateScene())
+                    {
+                        asyncOperation.allowSceneActivation = true;
+                    }
+                }
                 yield return null;
             }
         }
     }
 
-    public IEnumerator LoadSceneAsync(string sceneName, Action onLoading = null, Action onComplete = null)
+    public IEnumerator LoadSceneAsync(string sceneName, 
+        Func<bool> canActivateScene = null, Action onLoading = null, Action onComplete = null)
     {
         AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(sceneName);
         if (asyncOperation != null)
         {
             asyncOperation.allowSceneActivation = false;
+
             while (!asyncOperation.isDone)
             {
                 onLoading?.Invoke();
+
                 if (0.9f <= asyncOperation.progress)
                 {
                     onComplete?.Invoke();
-                    asyncOperation.allowSceneActivation = true;
+
+                    // 외부 조건이 true가 되어야 씬 전환 진행
+                    if (canActivateScene != null && canActivateScene())
+                    {
+                        asyncOperation.allowSceneActivation = true;
+                    }
                 }
                 yield return null;
             }
         }
     }
 
-    public IEnumerator LoadSceneAsync(ESceneNames sceneName, Action onLoading = null, Action onComplete = null)
+    public IEnumerator LoadSceneAsync(ESceneNames sceneName, 
+        Func<bool> canActivateScene = null, Action onLoading = null, Action onComplete = null)
     {
         AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(nameof(sceneName));
         if (asyncOperation != null)
         {
             asyncOperation.allowSceneActivation = false;
+
             while (!asyncOperation.isDone)
             {
                 onLoading?.Invoke();
+
                 if (0.9f <= asyncOperation.progress)
                 {
                     onComplete?.Invoke();
-                    asyncOperation.allowSceneActivation = true;
+
+                    // 외부 조건이 true가 되어야 씬 전환 진행
+                    if (canActivateScene != null && canActivateScene())
+                    {
+                        asyncOperation.allowSceneActivation = true;
+                    }
                 }
                 yield return null;
             }
