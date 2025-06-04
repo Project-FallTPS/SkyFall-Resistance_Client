6396b87ee9b9c68fcc90a130d35f2d23b46af87e
Sumin Cha <109404269+goliot@users.noreply.github.com>
2025-06-04 17:30:01 +0900

    Fix | 일단 지금은 정상

--- Assets/01. Sumin/02. Scripts/Player/AttackStrategies/Katana/Katana.cs
+++ Assets/01. Sumin/02. Scripts/Player/AttackStrategies/Katana/Katana.cs
@@ -20,6 +20,8 @@ public class Katana : MonoBehaviour
         {
             katanaStrategy.AccessoryOnAttack(); // 필요한 초기화 로직
         }
+
+        SoundManager.Instance.PlaySfx(ESfxType.PlayerSword1);
     }
 
     public void DisableAttack()
