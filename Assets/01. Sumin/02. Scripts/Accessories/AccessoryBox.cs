1632309da5bdd5ee7b271ef0cdda3d7bb6ee2381
wooing26 <104512612+wooing26@users.noreply.github.com>
2025-06-05 15:32:17 +0900

    Player Area Material

--- Assets/01. Sumin/02. Scripts/Accessories/AccessoryBox.cs
+++ Assets/01. Sumin/02. Scripts/Accessories/AccessoryBox.cs
@@ -51,7 +51,6 @@ public class AccessoryBox : MonoBehaviour
                     if(AccessoryPoolManager.Instance.GetObject(finalItem).TryGetComponent<IAccessory>(out var acc))
                     {
                         receiver.ReceiveAccessory(_type, acc);
-                        SoundManager.Instance.PlaySfx(ESfxType.Perk1);
                     }
                     break;
                 }
